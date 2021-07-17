3a3b33c5cf198b5cc9a772391dfca1d7ae21c9fe
souvenir718 <mongkey0@naver.com>
2021-07-17 13:30:10 +0900

    상태관리를 위한 TodoStore 작성(mobx)

---
+++ src/store/index.js
@@ -0,0 +1,4 @@
+import todoStore from "./todo";
+
+const store = { todoStore };
+export default store;
---
+++ src/store/todo.js
@@ -0,0 +1,66 @@
+import { makeAutoObservable } from "mobx";
+import moment from "moment";
+
+class Todo {
+    todoList = [
+        {
+            id: Math.random().toString(36).slice(2),
+            title: "title1",
+            content: "content1",
+            checked: false,
+            create_date: moment().format("YYYY.MM.DD"),
+        },
+        {
+            id: Math.random().toString(36).slice(2),
+            title: "title2",
+            content: "content2",
+            checked: false,
+            create_date: moment().format("YYYY.MM.DD"),
+        },
+        {
+            id: Math.random().toString(36).slice(2),
+            title: "title3",
+            content: "content3",
+            checked: false,
+            create_date: moment().format("YYYY.MM.DD"),
+        },
+        {
+            id: Math.random().toString(36).slice(2),
+            title: "프로그레시브 웹앱 끝내기",
+            content: "content4",
+            checked: true,
+            create_date: moment().format("YYYY.MM.DD"),
+        },
+        {
+            id: Math.random().toString(36).slice(2),
+            title: "title5",
+            content: "content5",
+            checked: true,
+            create_date: moment().format("YYYY.MM.DD"),
+        },
+    ];
+
+    todo = {
+        id: "",
+        title: "",
+        content: "",
+        checked: false,
+        create_date: moment().format("YYYY.MM.DD"),
+    };
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    addTodo = () => {};
+    toggleTodo = (id) => {
+        this.todoList = this.todoList.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo));
+    };
+    updateTodo = () => {};
+    deleteTodo = (id) => {
+        console.log(id);
+        this.todoList = this.todoList.filter((todo) => todo.id !== id);
+    };
+}
+const todoStore = new Todo();
+export default todoStore;
