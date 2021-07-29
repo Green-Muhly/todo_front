e2b0fe180ad14b7c3f2d39a32c48ddd9200be5f5
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-07-29 20:37:09 +0900

    [0729] tailwindCSS 적용

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -6,158 +6,182 @@ import EditIcon from "@material-ui/icons/Edit";
 Modal.setAppElement("#root");
 
 const customStyles = {
-    overlay: {
-        background: "rgba(0, 0, 0, 0.6)",
-    },
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        width: "85%",
-        height: "30%",
-        // borderRadius: "1rem",
-        boxShadow: "2px 2px 2px 0px rgba(0, 0, 0, 0.2)",
-        // border: "1px solid ",
-        padding: 10,
-        backgroundColor: "#fff",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
+  overlay: {
+    background: "rgba(0, 0, 0, 0.6)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "23rem",
+    height: "15rem",
+    // borderRadius: "1rem",
+    boxShadow: "2px 2px 2px 0px rgba(0, 0, 0, 0.2)",
+    // border: "1px solid ",
+    padding: 10,
+    backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 };
 
 const ModalHeader = styled.div`
-    height: 15%;
-    width: 95%;
-    /* background-color: rgb(125, 140, 255); */
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    margin-bottom: 1rem;
+  height: 15%;
+  width: 95%;
+  /* background-color: rgb(125, 140, 255); */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 1rem;
 `;
 const Title = styled.h2`
-    font-weight: 600;
-    font-size: 1.05rem;
-    font-family: sans-serif;
-    color: black;
-    width: 80%;
+  font-weight: 600;
+  font-size: 1.05rem;
+  font-family: sans-serif;
+  color: black;
+  width: 80%;
 `;
 const CloseBtn = styled.span`
-    cursor: pointer;
-    color: grey;
-    font-size: 1.6rem;
-    font-family: sans-serif;
+  cursor: pointer;
+  color: grey;
+  font-size: 1.6rem;
+  font-family: sans-serif;
 `;
 const ModalBody = styled.div`
-    background-color: white;
-    height: 80%;
-    width: 98%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    border-radius: 0.3rem;
+  background-color: white;
+  height: 80%;
+  width: 98%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 0.3rem;
 `;
 const Label = styled.div`
-    margin-left: 0.5rem;
-    /* background-color: red; */
-    font-size: 0.8rem;
-    font-weight: bolder;
-    color: gray;
-    margin-bottom: 0.5rem;
+  margin-left: 0.5rem;
+  /* background-color: red; */
+  font-size: 0.8rem;
+  font-weight: bolder;
+  color: gray;
+  margin-bottom: 0.5rem;
 `;
 const Span = styled.span`
-    margin-left: 0.5rem;
-    width: 90%;
-    border: none;
-    font-size: 0.9rem;
-    font-weight: bold;
-    line-height: 1.2rem;
-    margin-bottom: 1.5rem;
+  margin-left: 0.5rem;
+  width: 90%;
+  border: none;
+  font-size: 0.9rem;
+  font-weight: bold;
+  line-height: 1.2rem;
+  margin-bottom: 1.5rem;
 `;
 const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 `;
 const Button = styled.button`
-    width: 35%;
-    cursor: pointer;
-    border: 1px solid rgb(125, 140, 255);
-    border-radius: 0.3rem;
-    background-color: ${(props) => (props.save === true ? "rgb(125, 140, 255)" : "white")};
+  width: 35%;
+  cursor: pointer;
+  border: 1px solid rgb(125, 140, 255);
+  border-radius: 0.3rem;
+  background-color: ${(props) =>
+    props.save === true ? "rgb(125, 140, 255)" : "white"};
 
-    color: ${(props) => (props.save === true ? "white" : "rgb(125, 140, 255)")};
-    height: 2rem;
+  color: ${(props) => (props.save === true ? "white" : "rgb(125, 140, 255)")};
+  height: 2rem;
 `;
 
 const Input = styled.input`
-    margin-left: 0.5rem;
-    width: 90%;
-    border: none;
-    border-left: 2px solid #c2c2c2;
-    outline: none;
-    padding-left: 0.3rem;
+  margin-left: 0.5rem;
+  width: 90%;
+  border: none;
+  border-left: 2px solid #c2c2c2;
+  outline: none;
+  padding-left: 0.3rem;
+  font-size: 0.9rem;
+  line-height: 1.2rem;
+  margin-bottom: 1.5rem;
+  ::placeholder {
+    color: #c2c2c2;
+    /* font-weight: bold; */
     font-size: 0.9rem;
-    line-height: 1.2rem;
-    margin-bottom: 1.5rem;
-    ::placeholder {
-        color: #c2c2c2;
-        /* font-weight: bold; */
-        font-size: 0.9rem;
-    }
+  }
 `;
-const DetailModal = ({ isOpen, onRequestClose, title, content, id, create_date, onUpdate }) => {
-    const [isEdit, setIsEdit] = useState(false);
-    const [newTitle, setNewTitle] = useState(title);
-    const [newContent, setNewContent] = useState(content);
-    const setState = () => {
-        setIsEdit((prev) => !prev);
-    };
-    const onChange = (e) => {
-        const { name, value } = e.target;
-        if (name === "title") setNewTitle(value);
-        else if (name === "content") setNewContent(value);
-    };
-    const updateTodo = () => {
-        console.log(newTitle, newContent);
-        onUpdate(id, newTitle, newContent);
-        setIsEdit(false);
-        onRequestClose();
-    };
-    return (
-        <Modal style={customStyles} ariaHideApp={false} isOpen={isOpen} contentLabel="Selected Todo" onRequestClose={onRequestClose}>
-            <ModalHeader>
-                <Title>MY Task</Title>
-                {/* x버튼을 이미지 혹은 아이콘으로 대체할 방법 찾기 */}
-                <CloseBtn onClick={setState}>
-                    <EditIcon />
-                </CloseBtn>
-                <CloseBtn onClick={onRequestClose}>
-                    <CloseIcon />
-                </CloseBtn>
-            </ModalHeader>
-            <ModalBody>
-                <Label>TITLE</Label>
-                {isEdit ? <Input name="title" placeholder={title} onChange={onChange} /> : <Span>{title}</Span>}
+const DetailModal = ({
+  isOpen,
+  onRequestClose,
+  title,
+  content,
+  id,
+  create_date,
+  onUpdate,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
+  const [newContent, setNewContent] = useState(content);
+  const setState = () => {
+    setIsEdit((prev) => !prev);
+  };
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "title") setNewTitle(value);
+    else if (name === "content") setNewContent(value);
+  };
+  const updateTodo = () => {
+    console.log(newTitle, newContent);
+    onUpdate(id, newTitle, newContent);
+    setIsEdit(false);
+    onRequestClose();
+  };
+  return (
+    <Modal
+      className=""
+      style={customStyles}
+      ariaHideApp={false}
+      isOpen={isOpen}
+      contentLabel="Selected Todo"
+      onRequestClose={onRequestClose}
+    >
+      <ModalHeader>
+        <Title>MY Task</Title>
+        {/* x버튼을 이미지 혹은 아이콘으로 대체할 방법 찾기 */}
+        <CloseBtn onClick={setState}>
+          <EditIcon />
+        </CloseBtn>
+        <CloseBtn onClick={onRequestClose}>
+          <CloseIcon />
+        </CloseBtn>
+      </ModalHeader>
+      <ModalBody>
+        <Label>TITLE</Label>
+        {isEdit ? (
+          <Input name="title" placeholder={title} onChange={onChange} />
+        ) : (
+          <Span>{title}</Span>
+        )}
 
-                <Label>CONTENT</Label>
-                {isEdit ? <Input name="content" placeholder={content} onChange={onChange} /> : <Span>{content}</Span>}
-                <ButtonContainer>
-                    {isEdit ? (
-                        <Button onClick={updateTodo} save={true}>
-                            수정
-                        </Button>
-                    ) : (
-                        <></>
-                    )}
-                    <Button onClick={onRequestClose}>취소</Button>
-                </ButtonContainer>
-            </ModalBody>
-        </Modal>
-    );
+        <Label>CONTENT</Label>
+        {isEdit ? (
+          <Input name="content" placeholder={content} onChange={onChange} />
+        ) : (
+          <Span>{content}</Span>
+        )}
+        <ButtonContainer>
+          {isEdit ? (
+            <Button onClick={updateTodo} save={true}>
+              수정
+            </Button>
+          ) : (
+            <></>
+          )}
+          <Button onClick={onRequestClose}>취소</Button>
+        </ButtonContainer>
+      </ModalBody>
+    </Modal>
+  );
 };
 
 export default DetailModal;
--- src/pages/MainContainer.js
+++ src/pages/MainContainer.js
@@ -5,71 +5,78 @@ import components from "../components";
 import store from "../store";
 
 const Container = styled.div`
-    width: 90%;
-    height: 80%;
-    background-color: white;
-    border-radius: 3%;
-    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-    border: 0.1px solid rgba(0, 0, 0, 0.2);
-    display: flex;
-    flex-direction: column;
+  background-color: white;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  border: 0.1px solid rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
 `;
 const Top = styled.div`
-    height: 10%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-bottom: 1.5px solid rgba(125, 140, 255, 0.25);
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1.5px solid rgba(125, 140, 255, 0.25);
 `;
 const Mid = styled.div`
-    height: 80%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const Bot = styled.div`
-    height: 10%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    border-top: 1.5px solid rgba(125, 140, 255, 0.25);
+  height: 10%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 1.5px solid rgba(125, 140, 255, 0.25);
 `;
 
 const BotText = styled.span`
-    color: rgb(125, 140, 255);
-    font-size: 1rem;
-    font-weight: 600;
+  color: rgb(125, 140, 255);
+  font-size: 1rem;
+  font-weight: 600;
+  cursor: ${(props) => (props.btn === true ? "pointer" : null)};
 `;
 
 const MainContainer = observer(() => {
-    const { Today, Todolist, AddModal } = components;
-    const { todoStore } = store;
+  const { Today, Todolist, AddModal } = components;
+  const { todoStore } = store;
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const onModalClose = () => {
-        console.log(modalOpen);
-        setModalOpen(false);
-    };
-    const onModalOpen = () => {
-        console.log(modalOpen);
-        setModalOpen(true);
-    };
+  const [modalOpen, setModalOpen] = useState(false);
+  const onModalClose = () => {
+    console.log(modalOpen);
+    setModalOpen(false);
+  };
+  const onModalOpen = () => {
+    console.log(modalOpen);
+    setModalOpen(true);
+  };
 
-    return (
-        <Container>
-            <Top>
-                <Today />
-            </Top>
-            <Mid>
-                <Todolist />
-            </Mid>
-            <Bot>
-                <BotText>📃 Task : {todoStore.todoList.length ? todoStore.todoList.length : 0}</BotText>
-                <BotText onClick={onModalOpen}>Add Task 📝</BotText>
-                <AddModal onRequestClose={onModalClose} isOpen={modalOpen} createTodo={todoStore.createTodo} addTodo={todoStore.addTodo} />
-            </Bot>
-        </Container>
-    );
+  return (
+    <Container className="md:w-1/5 w-11/12 md:h-3/5 h-4/5 rounded-lg">
+      <Top>
+        <Today />
+      </Top>
+      <Mid>
+        <Todolist />
+      </Mid>
+      <Bot>
+        <BotText>
+          📃 Task : {todoStore.todoList.length ? todoStore.todoList.length : 0}
+        </BotText>
+        <BotText btn onClick={onModalOpen}>
+          Add Task 📝
+        </BotText>
+        <AddModal
+          onRequestClose={onModalClose}
+          isOpen={modalOpen}
+          createTodo={todoStore.createTodo}
+          addTodo={todoStore.addTodo}
+        />
+      </Bot>
+    </Container>
+  );
 });
 
 export default MainContainer;
