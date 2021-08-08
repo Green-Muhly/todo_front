edaa85779121e2d104e7aead57582b96410bffb9
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-08-09 05:43:41 +0900

    [0809] date값 수정

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -41,8 +41,7 @@ const dateFormat = (date) => {
   const year = date.getFullYear();
   const month =
     date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
-  const day =
-    date.getDate() + 1 < 10 ? `0${date.getDate() + 1}` : date.getDate() + 1;
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
 
   return `${year}.${month}.${day}`;
 };
